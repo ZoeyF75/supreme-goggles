42c217c3b7e72ff064e6c883dbc0b7447f78a421
ZoeyF75 <zf.zoodle@gmail.com>
2021-05-07 17:30:11 -0700

    get queries

--- server/index.js
+++ server/index.js
@@ -8,14 +8,48 @@ app.use(cors());
 app.use(express.json());
 
 //routes
-
 app.listen(5000, () => {
   console.log("server has started on port 5000");
 })
 
 //add to photos
-app.post("/phtos", async(req, res) => {
+app.post("/photos", async(req, res) => {
+  try {
+    const{ title , url } = req.body;
+    const newImage = await pool.query(`
+      INSERT INTO photos (title, url)
+      VALUES($1, $2)
+      RETURNING *;
+    `, [title, url])
+    res.json(newImage.rows[0]);
+  } catch (err) {
+      console.error(err.message);
+  }
+});
 
+//gets all saved photos
+app.get("/photos", (req, res) => {
+  try {
+    const allPhotos = await pool.query(`
+      SELECT * FROM photos;
+    `)
+    res.json(allPhotos.rows);
+  } catch (err) {
+      console.error(err.message);
+  }
+});
+
+//search/find specific image, change id to title potentially
+app.get("/photos/:id", async (req, res) => {
+  try {
+      const { id } = req.params;
+      const photo = await pool.query(`
+        SELECT * FROM photos WHERE id = $1
+      `, [id])
+      res.json(photo.rows[0]);
+  } catch (err) {
+      console.error(err.message);
+  }
 })
 
 //delete photos
