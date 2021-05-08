6ad76603047ba903c48003a3517f8c8573dff9cf
ZoeyF75 <zf.zoodle@gmail.com>
2021-05-07 17:08:48 -0700

    pg database introduced

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
-const cors = require("cors";)
+const cors = require("cors");
+const pool = require('./db');
 
 //middleware 
 app.use(cors());
 app.use(express.json());
 
+//routes
+
 app.listen(5000, () => {
   console.log("server has started on port 5000");
 })
+
+//add to photos
+app.post("/phtos", async(req, res) => {
+
+})
+
+//delete photos
