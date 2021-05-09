de334316860b8293f1fc4b377cddd22888618dcc
ZoeyF75 <zf.zoodle@gmail.com>
2021-05-08 19:38:10 -0700

    Delete function implemented

--- client/src/components/PhotosList.js
+++ client/src/components/PhotosList.js
@@ -18,6 +18,20 @@ const PhotosList = () => {
     getPhotos();
   }, []);
 
+  const deletePhoto = async(id) => {
+    console.log(id);
+    try {
+      console.log(id);
+      const deletePhoto = await fetch(`http://localhost:5000/photos/${id}`, {
+        method: "DELETE"
+      });
+      //returns all photos except one with id to be deleted
+      setPhotos(photos.filter(photo => photo.id !== id));
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
   return (
     <Fragment>
       <table class="table text-center mt-5">
@@ -28,10 +42,12 @@ const PhotosList = () => {
         </tr>
        
         {photos.map(photo => (
-           <tr>
+           <tr key={photo.id}>
             <td>{photo.title}</td>
             <td><a>{photo.url}</a></td>
             <td><img src={photo.url}></img></td>
+            <td>Edit</td>
+            <button className="btn btn-danger" onClick={() => deletePhoto(photo.id)}>Delete</button>
           </tr>
         ))}
       </table>
